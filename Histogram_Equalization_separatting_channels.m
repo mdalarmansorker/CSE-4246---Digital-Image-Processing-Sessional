45239218e3f490a1aa0d30f11715a2ba6a220b5e
MD Al Arman Sorker <73396847+mdalarmansorker@users.noreply.github.com>
2023-09-23 21:48:14 +0600

    Add files via upload

---
+++ Histogram_Equalization_separatting_channels.m
@@ -0,0 +1,30 @@
+% Read an image
+originalImage = imread('arman-bg.jpg'); % Replace with your image's filename
+
+% Convert the image to grayscale if it's not already
+grayImage = rgb2gray(originalImage); % Skip this step if your image is already grayscale
+
+% Get the number of intensity levels (usually 256 for grayscale)
+numIntensityLevels = 256;
+
+% Initialize an array to store histogram values
+histValues = zeros(1, numIntensityLevels);
+% Iterate through the image and count intensity occurrences
+[m, n] = size(grayImage);
+for i = 1:m
+    for j = 1:n
+        intensityValue = grayImage(i, j) + 1; 
+        histValues(intensityValue) = histValues(intensityValue) + 1;
+    end
+end
+
+subplot(1, 2, 1);
+imshow(originalImage);
+title('Original Image');
+
+% Plot the histogram
+subplot(1,2,2);
+bar(histValues);
+title('Histogram of Grayscale Image (Manual)');
+xlabel('Pixel Intensity');
+ylabel('Frequency');
